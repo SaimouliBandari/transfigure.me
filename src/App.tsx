1350e42744cba8c51a85114e58fc83908c18d100
saimoulibandari <saimouli4k@gmail.com>
2024-09-15 01:28:15 +0530

    removed unused code.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import { useState } from 'react'
 import { Outlet } from 'react-router-dom'
 import './App.scss'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
    <>
    <Outlet/>
